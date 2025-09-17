0abdc0b59d93969762ffe75cdb9d844e292abf6a
Felipe Domingues <felipeeu@yahoo.com.br>
2025-09-17 17:36:03 -0300

    updating repo

--- project.clj
+++ project.clj
@@ -1,15 +1,9 @@
-(defproject rest-api "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]
-                 [ring/ring-core "1.14.1"]
-                 [ring/ring-jetty-adapter "1.14.1"]
-                 [ring/ring-devel "1.14.1"]]
-  :ring {:handler sample/handler}
-  :plugins [[lein-ring "0.12.6"]]
-  :main ^:skip-aot rest-api.core
-  :target-path "target/%s"
-  :profiles {:uberjar {:aot :all
-                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
+(defproject rest_api "0.1.0-SNAPSHOT"
+  :description "Reitit Http App with Swagger"
+  :dependencies [[org.clojure/clojure "1.11.2"]
+                 [ring/ring-jetty-adapter "1.12.1"]
+                 [aleph "0.7.1"]
+                 [metosin/reitit "0.8.0"]
+                 [metosin/ring-swagger-ui "5.9.0"]]
+  
+  :repl-options {:init-ns rest_api.server})
--- src/rest_api/core.clj
+++
@@ -1,9 +0,0 @@
-(ns rest-api.core
-  (:gen-class))
-
-(defn handler [request]
-  {:status 200
-   :headers {"Content-Type" "text/html"}
-   :body "Hello World"})
-
-
